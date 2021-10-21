33220f93a6417746c7575fecae1efa23eadf62a7
unknown <eloso85@gmail.com>
2021-10-20 22:18:59 -0500

    yikes

--- server/index.js
+++ server/index.js
@@ -2,11 +2,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
-
 import dotenv from "dotenv"
 dotenv.config()
+
+//Routes
+import postRoutes from './routes/posts.js';
+
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 
 
