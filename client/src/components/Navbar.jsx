653d466c90b2bb8509788ea0476d1913c2fee712
unknown <eloso85@gmail.com>
2021-10-24 16:12:25 -0500

    working on bottom finshed searchbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ListGroup from 'react-bootstrap/ListGroup'
+// import ListGroup from 'react-bootstrap/ListGroup'
 import Badge from 'react-bootstrap/Badge'
 import 'boxicons'
 import { Link } from 'react-router-dom';
@@ -20,79 +20,82 @@ const Navbar = () => {
                         <box-icon color="white" name='menu' id="btn" ></box-icon>
                     </div>
                     <ul className="nav-list list-unstyled">
-                        <li>
+                        <li className="search">
 
-                            <box-icon color="white" name='search' ></box-icon>
+                            <box-icon color="white" name='search' className="search"></box-icon>
                             <input type="text" placeholder="Search..." />
                             <span className="tooltip">Search</span>
                         </li>
                         <li>
-                            <a href="#">
-                                <box-icon color="white" name='grid-alt'></box-icon>
+                            <Link to="/">
+                                <box-icon color="white" name='grid-alt' animation="flashing-hover"></box-icon>
                                 <span className="links_name">Dashboard</span>
-                            </a>
+                            </Link>
                             <span className="tooltip">Dashboard</span>
                         </li>
                         <li>
-                            <a href="#">
+                            <Link to="/">
                                 <box-icon color="white" name='user'></box-icon>
                                 <span className="links_name">User</span>
-                            </a>
+                            </Link>
                             <span className="tooltip">User</span>
                         </li>
                         <li>
-                            <a href="#">
+                            <Link to="/">
                                 <box-icon color="white" name='chat'></box-icon>
                                 <span className="links_name">Messages</span>
-                            </a>
+                            </Link>
                             <span className="tooltip">Messages</span>
                         </li>
                         <li>
-                            <a href="#">
+                            <Link to="/">
                                 <box-icon color="white" name='pie-chart-alt-2' ></box-icon>
                                 <span className="links_name">Analytics</span>
-                            </a>
+                            </Link>
                             <span className="tooltip">Analytics</span>
                         </li>
                         <li>
-                            <a href="#">
+                            <Link to="/">
                                 <box-icon color="white" name='folder' ></box-icon>
                                 <span className="links_name">File Manager</span>
-                            </a>
+                            </Link>
                             <span className="tooltip">Files</span>
                         </li>
                         <li>
-                            <a href="#">
+                            <Link to="/">
                                 <box-icon color="white" name='cart-alt' ></box-icon>
                                 <span className="links_name">Order</span>
-                            </a>
+                            </Link>
                             <span className="tooltip">Order</span>
                         </li>
                         <li>
-                            <a href="#">
+                            <Link to="/">
                                 <box-icon color="white" name='heart' ></box-icon>
                                 <span class="links_name">Saved</span>
-                            </a>
+                            </Link>
                             <span class="tooltip">Saved</span>
                         </li>
                         <li>
-                            <a href="#">
+                            <Link to="/">
                                 <box-icon color="white" name='cog' ></box-icon>
                                 <span class="links_name">Setting</span>
-                            </a>
+                            </Link>
                             <span class="tooltip">Setting</span>
                         </li>
-                        <li class="profile">
-                            <div class="profile-details">
-                                {/* <img src="profile.jpg" alt="profileImg"/> */}
+                        
+                    </ul>
+                    <div class="profile_content">
+                            <div className="profile">
+                            <div class="profile_details">
+                                <img src={icon} alt="profileImg"/>
                                 <div class ="name_job">
                                 <div class ="name">Prem Shahi</div>
                                 <div class ="job">Web designer</div>
                                 </div>
+                                </div>
                             </div>
                             <box-icon color="white" name='log-out' id="logout"></box-icon>
-                        </li>
-                    </ul>
+                        </div>
                 </div>
             </Nav>
         </>
